d4776d67c272298ca14c75612a9f5c1aa5626d09
Tyler Stegmaier <jakar510@gmail.com>
2021-01-19 11:01:27 -0600

    bug fixes / updates

--- PythonExtensions/__version__.py
+++ PythonExtensions/__version__.py
@@ -9,6 +9,6 @@
 # ------------------------------------------------------------------------------
 
 # major.minor[.maintenance]
-VERSION = (1, 0, 13)
+VERSION = (1, 0, 14)
 
 version = '.'.join(map(str, VERSION))
